951ecc7cb604e98e1fffbcf8c39734096f2b5f02
Çağatay Yürük <cagatayyuruk@gmail.com>
2016-08-23 15:48:46 +0300

    Add files via upload
    Describe the code

--- read_serial_1.py
+++ read_serial_1.py
@@ -3,13 +3,14 @@ import time
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+#gspread protokol tanımlamaları
 scope = ['https://spreadsheets.google.com/feeds']
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name('demirdokumuts-5693c98232f3.json', scope)
 
 gc = gspread.authorize(credentials)
 
-
+#seri haberleşme tanımlamaları
 ser = serial.Serial('/dev/ttyUSB0') # open the COM Port
 ser.baudrate = 19200          # set Baud rate
 ser.bytesize = 8             # Number of data bits = 8
@@ -18,7 +19,7 @@ ser.parity   = 'N'           # No parity
 ser.stopbits = 1             # Number of Stop bits = 1
 
 
-
+#seri haberleşmeden gelen veriyi okuyan fonksiyon
 def readline(a_serial, eol=b'\r'):
     leneol = len(eol)
     line = bytearray()
@@ -38,10 +39,10 @@ wks = gc.open("sheet1").sheet1
 
 i=0
 while i<10:
-    data = readline(ser)
-    print (data)  
-    print ("veri numarası " + str(i))
-    wks.update_cell(1+i,2, data)
+    data = readline(ser)        #veriyi oku
+    print (data)                #shell e veriyi yaz
+    print ("veri numarası " + str(i))       
+    wks.update_cell(1+i,2, data)        #drive'daki dosyanın 1. sütun i+1. satırını değiştir
     time.sleep(10)
     i += 1
     
