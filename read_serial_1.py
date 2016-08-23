0ef9eaa14655e6a0107707768b7da0cc55116cbb
Çağatay Yürük <cagatayyuruk@gmail.com>
2016-08-23 15:42:41 +0300

    Add files via upload
    gspread library added, log data to google spread sheet from card

--- read_serial_1.py
+++ read_serial_1.py
@@ -1,7 +1,16 @@
 import serial                    # import pySerial module
-import sys
+import time
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-ser = serial.Serial('COM3') # open the COM Port
+scope = ['https://spreadsheets.google.com/feeds']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('demirdokumuts-5693c98232f3.json', scope)
+
+gc = gspread.authorize(credentials)
+
+
+ser = serial.Serial('/dev/ttyUSB0') # open the COM Port
 ser.baudrate = 19200          # set Baud rate
 ser.bytesize = 8             # Number of data bits = 8
 ser.timeout = 1
@@ -9,14 +18,13 @@ ser.parity   = 'N'           # No parity
 ser.stopbits = 1             # Number of Stop bits = 1
 
 
-   
+
 def readline(a_serial, eol=b'\r'):
     leneol = len(eol)
     line = bytearray()
     while True:
         c = a_serial.read(1)
         if c:
-            print (type(c))
             line += c
             if line[-leneol:] == eol:
                 break
@@ -24,15 +32,20 @@ def readline(a_serial, eol=b'\r'):
             break
     return line
 
+# Open a worksheet from spreadsheet with one shot
+wks = gc.open("sheet1").sheet1
+
 
 i=0
 while i<10:
     data = readline(ser)
- 
-    print (data)
-    i += 1
+    print (data)  
     print ("veri numarası " + str(i))
+    wks.update_cell(1+i,2, data)
+    time.sleep(10)
+    i += 1
     
-    
+
+
         
 ser.close()      
